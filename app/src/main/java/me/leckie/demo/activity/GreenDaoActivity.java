4d60e6e08a835c9cd65bc965fac3f88e89c93001
Leckie <603418904@qq.com>
2015-06-18 18:13:44 +0800

    use ListActivity and SimpleCursorAdapter

--- app/src/main/java/me/leckie/demo/activity/GreenDaoActivity.java
+++ app/src/main/java/me/leckie/demo/activity/GreenDaoActivity.java
@@ -1,9 +1,12 @@
 package me.leckie.demo.activity;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.Date;
 import java.util.List;
@@ -14,15 +17,15 @@ import me.leckie.demo.greendao.DaoSession;
 import me.leckie.demo.greendao.Note;
 import me.leckie.demo.greendao.NoteDao;
 
-public class GreenDaoActivity extends Activity {
-
-    private TextView mTextView;
+public class GreenDaoActivity extends ListActivity {
 
     private SQLiteDatabase db;
 
     private DaoMaster daoMaster;
     private DaoSession daoSession;
     private NoteDao noteDao;
+
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +35,38 @@ public class GreenDaoActivity extends Activity {
         init();
 
         addNote();
-
-        query();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        deleteAll();
+        //deleteAll();
     }
 
     public void init() {
-        mTextView = (TextView) findViewById(R.id.tv);
-
         DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes.db", null);
         db = helper.getWritableDatabase();
         daoMaster = new DaoMaster(db);
         daoSession = daoMaster.newSession();
         noteDao = daoSession.getNoteDao();
+
+        cursor = db.query(noteDao.getTablename(), noteDao.getAllColumns(), null, null, null, null, null);
+
+        String[] from = {NoteDao.Properties.Text.columnName, NoteDao.Properties.Comment.columnName};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to);
+        setListAdapter(adapter);
     }
 
     public void addNote() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 6; i++) {
             String text = "notes txt " + i;
             String comment = "comment txt " + i;
             Note note = new Note(null, text, comment, new Date());
             noteDao.insert(note);
         }
+        cursor.requery();
     }
 
     public void query() {
@@ -70,7 +78,6 @@ public class GreenDaoActivity extends Activity {
             sb.append(note.getComment() + ",");
             sb.append(note.getDate() + ",");
         }
-        mTextView.setText(sb);
     }
 
     public void deleteAll() {
