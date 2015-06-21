80c25719b13bd24aae50648bcc3b12cd01eb96c3
Leckie <603418904@qq.com>
2015-06-21 19:44:59 +0800

    simple SlidingMenu

--- app/src/main/java/me/leckie/demo/activity/ListViewActivity.java
+++ app/src/main/java/me/leckie/demo/activity/ListViewActivity.java
@@ -21,12 +21,12 @@ public class ListViewActivity extends Activity {
         // items
         String[] items = {"Back", "Lifecycle", "GridViewActivity",
                 "ListViewLoader", "Settings", "Fragment",
-                "DrawBall", "Movement", "SurfaceView", "Moveable", "GreenDao"};
+                "DrawBall", "Movement", "SurfaceView", "Moveable", "GreenDao", "SlidingMenu"};
         // Demo class
         final Class[] targets = {MainActivity.class, LifecycleActivity.class, GridViewActivity.class,
                 ListViewLoader.class, SettingsActivity.class, FragmentActivity.class,
                 DrawBallActivity.class, MovementActivity.class, SurfaceActivity.class,
-                MoveableActivity.class, GreenDaoActivity.class};
+                MoveableActivity.class, GreenDaoActivity.class, SlidingMenuActivity.class};
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
         listView = (ListView) findViewById(R.id.list_item);
---
+++ app/src/main/java/me/leckie/demo/activity/SlidingMenuActivity.java
@@ -0,0 +1,15 @@
+package me.leckie.demo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import me.leckie.demo.R;
+
+public class SlidingMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding_menu);
+    }
+}
